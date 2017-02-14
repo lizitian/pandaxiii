2b96e6eb5f5ec6856a8a9f9c149502a4a3207a52
Zitian Li <lizitian@mail.ustc.edu.cn>
2017-02-14 19:48:06 +0800

    add callback functions

--- rbcp.cpp
+++ rbcp.cpp
@@ -8,6 +8,18 @@ int main(int argc, char *argv[])
     return app.exec();
 }
 
+void MainWindow::on_write_clicked(bool checked)
+{
+    ui.output->moveCursor(QTextCursor::End);
+    ui.output->insertPlainText("write clicked\n");
+}
+
+void MainWindow::on_read_clicked(bool checked)
+{
+    ui.output->moveCursor(QTextCursor::End);
+    ui.output->insertPlainText("read clicked\n");
+}
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     ui.setupUi(this);
--- rbcp.h
+++ rbcp.h
@@ -6,6 +6,9 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = 0);
+private slots:
+    void on_write_clicked(bool checked);
+    void on_read_clicked(bool checked);
 private:
     Ui::MainWindow ui;
 };
